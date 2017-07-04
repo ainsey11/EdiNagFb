39bea177f13c8e17fdf199b258350d1f1efdcf3c
ainsey11 <robert@ainsey11.com>
2017-07-05 00:39:21 +0100

    Added password config validation too

--- FBNotifier.py
+++ FBNotifier.py
@@ -2,7 +2,7 @@ from fbchat import Client
 from fbchat.models import *
 from ConfigParser import SafeConfigParser
 import logging
-
+import sys
 
 config = SafeConfigParser()
 config.read ('config.ini')
@@ -12,8 +12,18 @@ logging.basicConfig(filename='EdiNagFBDebug.log',level=logging.DEBUG)
 
 fbuseremail = config.get("FacebookAuthDetails","Email")
 fbuserpassword = config.get("FacebookAuthDetails","Password")
+configerrormessage = config.get("ErrorMessages","ConfigError")
 
-print(fbuseremail)
+logging.info('Loaded Configuration')
+logging.debug('Performing Configuration Validation')
+
 if fbuseremail == 'FBEmailAddress':
-    print("You have not set an e-mail address in the config.ini, exiting")
-#    logging.debug('Email not set in config.ini, exiting.')
+    print(configerrormessage)
+    logging.warning('Email not set in config.ini')
+    sys.exit(1)
+
+if fbuserpassword == 'BlankPassword':
+    print(configerrormessage)
+    logging.warning('Password not set in config.ini')
+    sys.exit(1)
+
