cdface620ff56055fa7a05f3bab1f04b458e4a92
ainsey11 <robert@ainsey11.com>
2017-07-04 23:58:37 +0100

    adding in username / password prompt

--- FBNotifier.py
+++ FBNotifier.py
@@ -1,4 +1,7 @@
 from fbchat import Client
 from fbchat.models import *
 
+fbuseremail = input('Please Enter your Facebook e-mail address')
+fbuserpassword = input('Please Enter your Facebook password')
 
+print('Using' $fbuseremail 'as username')
